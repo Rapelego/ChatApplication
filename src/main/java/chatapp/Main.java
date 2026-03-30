8cabb085d19b124004e401f459df321b0ec5a1b8
LethaboMaditsi <st10496347@rcconnect.edu.za>
2026-03-30 09:24:21 +0200

    adding Junit

--- src/main/java/chatapp/Main.java
+++ src/main/java/chatapp/Main.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package chatapp;
 
-/**
- *
- * @author User
- */
+import java.util.Scanner;
+
 public class Main {
-    
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        Login login = new Login();
+
+        // USER DETAILS
+        System.out.print("Enter first name: ");
+        String firstName = input.nextLine();
+
+        System.out.print("Enter last name: ");
+        String lastName = input.nextLine();
+
+        // REGISTRATION
+        System.out.print("Enter username: ");
+        String username = input.nextLine();
+
+        System.out.print("Enter password: ");
+        String password = input.nextLine();
+
+        System.out.print("Enter cell phone (+27...): ");
+        String cell = input.nextLine();
+
+        String registerMessage = login.registerUser(username, password, cell);
+        System.out.println(registerMessage);
+
+        // LOGIN
+        System.out.print("Enter username to login: ");
+        String loginUser = input.nextLine();
+
+        System.out.print("Enter password to login: ");
+        String loginPass = input.nextLine();
+
+        boolean loginStatus = login.loginUser(loginUser, loginPass);
+
+        System.out.println(login.returnLoginStatus(loginStatus, firstName, lastName));
+    }
 }
